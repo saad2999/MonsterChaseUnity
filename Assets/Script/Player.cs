e71cd1a26eb71d4e62ef163dba2e535a141bfca0
DESKTOP-DVQ765F\Maaz <saad586305@gmail.com>
2022-09-14 20:17:40 +0500

    Script update [veriables added]

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField]
+    private float moveForce = 22f;
+    [SerializeField]
+    private float jumpForce = 11f;
+    private float moveX;
+    private Rigidbody2D mybody2d;
+    private Animator anim;
+    private string WalkAnimation="Walk";
     // Start is called before the first frame update
     void Start()
     {
